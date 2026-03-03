a47badec84e7ffacc8bbbab95e7784b869ad043a
Erik Lima <erikhalima05@gmail.com>
2026-03-03 17:04:43 -0300

    fix: implement whole main code (if you're a recruiter I'm sorry, but I got really into it and forgot to commit the changes)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,61 @@
-import * as fs from "fs";
-import { parse } from "csv-parse/sync";
+import { createReadStream } from "fs";
+import { parse } from "csv-parse";
 
-const results = parse(fs.readFileSync("data.csv"));
-console.log(Object.getPrototypeOf(results));
+import { formatBytes } from "./utils";
 
-const setResults = new Set(results);
-console.log(Object.getPrototypeOf(setResults));
+/**
+ * Reads a CSV file using a Readable Stream, counts the occurrences
+ * of each ID,and logs the total unique IDs, total duplicates, and
+ * the duplicate IDs with their counts.
+ * @param path Target CSV file's path
+ * @returns An Iterator of entries containing duplicate IDs and their counts.
+ */
+async function streamMap(path: string = "./data.csv") {
+  const parser = createReadStream(path).pipe(parse({ fromLine: 2 }));
+
+  // Map to count occurrences of each ID
+  const counter = new Map<string, number>();
+
+  for await (const record of parser) {
+    const id = record[0];
+    counter.set(id, (counter.get(id) ?? 0) + 1);
+  }
+
+  // Returns only duplicates
+  // Ps.: Using directly entries() avoids excess of
+  // memory usage and optimizes time complexity,
+  // since we don't need to create an intermediate
+  // array of keys or values.
+  return counter.entries().filter((v) => v[1] > 1);
+}
+
+/**
+ * Logs the memory usage and time taken for the callback to execute.
+ * @param callback Target function to measure memory usage and execution time.
+ * @returns The result of the callback function.
+ */
+async function logUsage<T>(callback: () => Promise<T>): Promise<T> {
+  const m0 = process.memoryUsage().heapUsed;
+  const t0 = performance.now();
+
+  const data = await callback();
+
+  const m1 = process.memoryUsage().heapUsed;
+  const t1 = performance.now();
+
+  console.log(`Memory usage: ${formatBytes(m1 - m0)}`);
+  console.log(`Time taken: ${(t1 - t0).toFixed(2)} ms`);
+
+  return data;
+}
+
+logUsage(() => streamMap("./data.csv")).then((duplicates) => {
+  const duplicatesArray = Array.from(duplicates);
+  console.log(
+    "Duplicates:",
+    duplicatesArray.reduce((acc, [id, count]) => `${acc}, ${id}(${count})`, ""),
+  );
+  console.log(
+    `Total duplicates occurences: ${duplicatesArray.reduce((acc, [_, count]) => acc + count, 0)}`,
+  );
+});
